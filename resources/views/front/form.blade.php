bace3f0e18dd3d4e7e7fbd50923b2ea7cc996f7a
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-08-14 11:07:43 +0100

    dbg 4

--- resources/views/front/form.blade.php
+++ resources/views/front/form.blade.php
@@ -21,10 +21,7 @@
                             <input type="text" name="firstname" class="form-control" id="input-firstname" required>
                             <label for="input-firstname">Prénom</label>
                         </div>
-                        <div class="form-group">
-                            <input type="text" name="firstname" class="form-control" id="input-firstname">
-                            <label for="input-firstname">Prénom</label>
-                        </div>
+
                         <div class="form-group">
                             <input type="tel" name="phone" class="form-control" id="input-phone" required>
                             <label for="input-phone">Téléphone</label>
